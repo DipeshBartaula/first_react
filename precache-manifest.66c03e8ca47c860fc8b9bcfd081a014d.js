0c15cfe12d940f7e2b93f131529c882e5b4baa9b
DipesBartaula <dipeshbartaula4@gmail.com>
2022-05-04 21:43:08 +0545

    Updates

---
+++ precache-manifest.66c03e8ca47c860fc8b9bcfd081a014d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea5b091092d53fff36c8",
+    "url": "/first_react/static/css/main.a143ff9b.chunk.css"
+  },
+  {
+    "revision": "ea5b091092d53fff36c8",
+    "url": "/first_react/static/js/main.1b15a70c.chunk.js"
+  },
+  {
+    "revision": "6856a2f495259110a210",
+    "url": "/first_react/static/js/runtime~main.dc3f6a5a.js"
+  },
+  {
+    "revision": "62c0c68076f7d9b8d3e2",
+    "url": "/first_react/static/css/2.aa04fc2a.chunk.css"
+  },
+  {
+    "revision": "62c0c68076f7d9b8d3e2",
+    "url": "/first_react/static/js/2.e009e008.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/first_react/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/first_react/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "71e2fe7f994af3ba34d64f9cf42693ae",
+    "url": "/first_react/index.html"
+  }
+];
